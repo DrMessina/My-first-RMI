350fc15673419960a51d9bab7e7b36b8321c7787
HDebeuf <32390605+HDebeuf@users.noreply.github.com>
2017-11-02 22:09:33 +0100

    méthode addRoom
    AddRoom casté depuis le GUI vers le client et puis serveur

--- src/ClientRMI.java
+++ src/ClientRMI.java
@@ -1,14 +1,28 @@
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public class ClientRMI {
 	
 	GUIInterface chatGUI;
-	
+	InterfaceChatSRV serverInterface;
+
 public ClientRMI() {
 	chatGUI = new Chat_GUI();
 	
 	javax.swing.SwingUtilities.invokeLater(new Runnable() {
         public void run() {
         	chatGUI.createInputInterface("login");
+        	try {
+				 serverInterface = (InterfaceChatSRV) Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/TestRMI");
+			} catch (MalformedURLException | RemoteException | UnknownHostException | NotBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
     });
 }
@@ -23,6 +37,17 @@ public static void main(String[] argv) {
 	}
 		
 	}
+
+public void addRoom(String userName, int id) {
+	User user = new User(userName);
+	
+	try {
+		serverInterface.addRoom(user, id);
+	} catch (RemoteException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 
 	/*public Chat_client() {
 		
